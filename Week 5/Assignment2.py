411d8b2a5a4fc8ce486916c01f71345c37e7c77c
Mayur Pai B H <mayur5pai@gmail.com>
2020-10-19 16:13:05 +0530

    Assignment2

---
+++ Week 5/Assignment2.py
@@ -0,0 +1,18 @@
+import math
+n = int(input())
+move = []
+originX = 0
+originY = 0
+
+for i in range(n):
+  direction,step = input().split()
+  if direction == 'UP':
+    originY = originY + int(step)
+  if direction == 'DOWN':
+    originY = originY - int(step)
+  if direction == 'LEFT':
+    originX = originX - int(step)
+  if direction == 'RIGHT':
+    originX = originX + int(step)
+
+print(round(math.sqrt(originX*originX+originY*originY)),end="")
