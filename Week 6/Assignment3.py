47f92bc18a63a078d23eb8f5f7cfe1226ffb90ed
Mayur Pai B H <mayur5pai@gmail.com>
2020-10-20 20:20:34 +0530

    Week6

---
+++ Week 6/Assignment3.py
@@ -0,0 +1,17 @@
+matrix = []
+n = int(input())
+for i in range(n):
+    a = []
+    for j in range(n):
+        a.append(int(input()))
+    matrix.append(a)
+
+for i in range(n):
+    for j in range(n):
+        if i<j:
+            matrix[i][j] = 0
+            
+for i in range(n):
+    for j in range(n):
+        print(matrix[i][j],end=" ")
+    print()
