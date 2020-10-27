b7a6967a9081b21036f78ccfcc5b354dfffd32f0
Mayur Pai B H <mayur5pai@gmail.com>
2020-10-27 11:01:38 +0530

    Week6

--- Week 6/Assignment3.py
+++ Week 6/Assignment3.py
@@ -1,17 +1,24 @@
-matrix = []
-n = int(input())
+# Programming Assignment 3: Lower Triangular Matrix
+n=int(input())
+matrix=[]
 for i in range(n):
-    a = []
-    for j in range(n):
-        a.append(int(input()))
-    matrix.append(a)
+    for j in range(1):
+        ele=[int(a) for a in input().split()]
+        matrix.append(ele)
+
 
 for i in range(n):
     for j in range(n):
         if i<j:
-            matrix[i][j] = 0
-            
-for i in range(n):
-    for j in range(n):
-        print(matrix[i][j],end=" ")
-    print()
+            if j==n-1:
+                print(0,end="")
+            else:
+                print(0,end=" ")
+        else:
+            if j==n-1:
+                print(matrix[i][j],end="")
+            else:
+                print(matrix[i][j],end=" ")
+        
+    if i!=n-1:
+      print()
